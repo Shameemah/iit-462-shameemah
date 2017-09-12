c77b20229d3fdcf1cfdfd42f0f925533a7785f66
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-09-12 12:42:29 -0500

    Refactored function to get value from text input and populate comment section on button click

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -1,5 +1,7 @@
-$(".comment-input input").on("click", function(){
-  var $newP = $("<p>");
+$(".comment-input input[type='button']").on("click", function(){
+  var $newP = $("<p>"),
+      comment_text = $(".comment-input input[type='text']").val();
+  $newP.text(comment_text);
   $(".comments").append($newP);
 });
 $(document).ready();
