fdcc88b03956f83dfc2d8d669533eef2abeb1ec3
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-09-12 12:51:01 -0500

    Refactored function to clear text box after submission

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -1,7 +1,11 @@
 $(".comment-input input[type='button']").on("click", function(){
-  var $newP = $("<p>"),
-      comment_text = $(".comment-input input[type='text']").val();
-  $newP.text(comment_text);
-  $(".comments").append($newP);
+  var $newComment = $("<p>"), $comment_text = $(".comment-input input[type='text']");
+
+  if($comment_text.val() !== "") {
+    $newComment.text($comment_text.val());
+    $(".comments").append($newComment);
+    $comment_text.val("");
+  }
+  
 });
 $(document).ready();
