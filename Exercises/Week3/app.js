5f4923e4c4a34152f2ab0fa6ed9f407b9e0cdc97
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-09-12 13:03:33 -0500

    Refactored to reduce duplication

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -1,4 +1,4 @@
-$(".comment-input input[type='button']").on("click", function(){
+var addCommentFromInputBox = function() {
   var $newComment = $("<p>"), $comment_text = $(".comment-input input[type='text']");
 
   if($comment_text.val() !== "") {
@@ -6,6 +6,17 @@ $(".comment-input input[type='button']").on("click", function(){
     $newComment.fadeIn(100);
     $(".comments").append($newComment);
     $comment_text.val("");
+  }
+}
+
+$(".comment-input input[type='button']").on("click", function(){
+  addCommentFromInputBox();
+});
+
+$(".comment-input input[type='button']").on("keypress", function(event){
+
+  if(event.keyCode == 13) {
+    addCommentFromInputBox();
   }
 });
 $(document).ready();
