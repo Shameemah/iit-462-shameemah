da1afd4992777319a1190ced183d8cc161636c54
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-10-10 17:05:02 -0500

    Completed GET method

--- Assignments/Week7/server.js
+++ Assignments/Week7/server.js
@@ -1,8 +1,24 @@
-var http = require("http");
+var express = require("express");
+var mongodb = require('mongodb');
+var ObjectId = require('mongodb').ObjectID;
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+var app = express();
 
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello World\n');
-}).listen(3000);
+const port = 8000;
 
-console.log('Server running at port 3000');
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
+
+app.get("/hands/:id", function(req, res) {
+
+  MongoClient.connect('mongodb://localhost:27017/hands', function (err, db) {
+
+    db.collection('hands').find({"_id" : ObjectId(req.params.id)}).toArray(function (err, result) {
+      if (err) console.log(err);
+      if (result.length < 1) { res.status(404).send("Not found.");}
+      if(result) { res.status(200).send(result);}
+    })
+  })
+});
