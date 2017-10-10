936f5b4d5933db1b2b6411f7dfa789b28cc5ed2d
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-10-10 17:35:13 -0500

    Completed get request for cards

--- Assignments/Week7/server.js
+++ Assignments/Week7/server.js
@@ -22,3 +22,15 @@ app.get("/hands/:id", function(req, res) {
     })
   })
 });
+
+app.get("/hands/:id/cards", function(req, res) {
+
+  MongoClient.connect('mongodb://localhost:27017/hands', function (err, db) {
+
+    db.collection('hands').find({"_id" : ObjectId(req.params.id)}).toArray(function (err, result) {
+      if (err) console.log(err);
+      if (result.length < 1) { res.status(404).send("Not found.");}
+      if(result) { res.status(200).send(result[0]["cards"]);}
+    })
+  })
+});
