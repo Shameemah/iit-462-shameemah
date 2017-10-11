b4f24f1eb9012c3b5333513bf8493cf28cfa2d69
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-10-10 20:33:10 -0500

    Second get request is working with hands array

--- Assignments/Week7/server.js
+++ Assignments/Week7/server.js
@@ -6,6 +6,7 @@ app.listen(port, function () {
   console.log('Server is running on port ' + port);
 })
 
+var id;
 var hands =
   [
     {
@@ -34,19 +35,19 @@ var hands =
 
 app.get("/hands/:id", function(req, res) {
 
-    var id = req.params.id;
+    id = req.params.id;
 
     if (!hands[id]) { res.status(404).send("Error 404 - Not found");}
     res.status(200).send(hands[id]);
 })
 
-// app.get("/hands/:id/cards", function(req, res) {
-//     db.collection('hands').find({"_id" : ObjectId(req.params.id)}).toArray(function (err, result) {
-//       if (err) console.log(err);
-//       if (result.length < 1) { res.status(404).send("Not found.");}
-//       if(result) { res.status(200).send(result[0]["cards"]);}
-//     })
-// });
+app.get("/hands/:id/cards", function(req, res) {
+
+    id = req.params.id;
+
+    if (!hands[id]) { res.status(404).send("Error 404 - Not found");}
+    res.status(200).send(hands[id]["cards"]);
+})
 //
 // app.post('/hands', function (req, res) {
 //     db.collection('hands').insertOne(post_params, function (err, result) {
