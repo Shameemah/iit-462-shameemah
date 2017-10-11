6c5924e6c36c294d773e8f238a5a6624e79aaea5
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-10-10 20:30:24 -0500

    First get request working with hands array

--- Assignments/Week7/server.js
+++ Assignments/Week7/server.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var app = express();
+const port = 8000;
+
+app.listen(port, function () {
+  console.log('Server is running on port ' + port);
+})
 
 var hands =
   [
     {
-      "id" : "1",
+      "id" : "0",
       "cards" :
       [
         {"suit" : "diamonds", "rank" : "4"},
@@ -15,7 +20,7 @@ var hands =
       ]
     },
     {
-      "id" : "2",
+      "id" : "1",
       "cards" :
       [
         {"suit" : "spades", "rank" : "k"},
@@ -27,40 +32,26 @@ var hands =
     }
   ]
 
-
-MongoClient.connect('mongodb://localhost:27017/hands', function (err, database) {
-  if (err)
-    throw err
-  else
-    {
-      db = database;
-      console.log("Connected to MongoDB");
-      app.listen(port);
-      console.log("Listening on port " + port)
-    }
-});
-
-
 app.get("/hands/:id", function(req, res) {
-    db.collection('hands').find({"_id" : ObjectId(req.params.id)}).toArray(function (err, result) {
-      if (err) console.log(err);
-      if (result.length < 1) { res.status(404).send("Not found.");}
-      if(result) { res.status(200).send(result);}
-    })
-});
 
-app.get("/hands/:id/cards", function(req, res) {
-    db.collection('hands').find({"_id" : ObjectId(req.params.id)}).toArray(function (err, result) {
-      if (err) console.log(err);
-      if (result.length < 1) { res.status(404).send("Not found.");}
-      if(result) { res.status(200).send(result[0]["cards"]);}
-    })
-});
+    var id = req.params.id;
 
-app.post('/hands', function (req, res) {
-    db.collection('hands').insertOne(post_params, function (err, result) {
-      if (err) console.log(err);
-      console.log(result);
-      res.status(200).send(result[0]["_id"]);
-    })
-});
+    if (!hands[id]) { res.status(404).send("Error 404 - Not found");}
+    res.status(200).send(hands[id]);
+})
+
+// app.get("/hands/:id/cards", function(req, res) {
+//     db.collection('hands').find({"_id" : ObjectId(req.params.id)}).toArray(function (err, result) {
+//       if (err) console.log(err);
+//       if (result.length < 1) { res.status(404).send("Not found.");}
+//       if(result) { res.status(200).send(result[0]["cards"]);}
+//     })
+// });
+//
+// app.post('/hands', function (req, res) {
+//     db.collection('hands').insertOne(post_params, function (err, result) {
+//       if (err) console.log(err);
+//       console.log(result);
+//       res.status(200).send(result[0]["_id"]);
+//     })
+// });
