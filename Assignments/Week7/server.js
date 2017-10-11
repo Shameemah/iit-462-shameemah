c6a21c1a33d46d3905b657615a5bbdf3e695b527
Shameemah <Shameemah@MacBook-Pro-2.local>
2017-10-10 23:33:32 -0500

    Completed put request

--- Assignments/Week7/server.js
+++ Assignments/Week7/server.js
@@ -1,16 +1,23 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser')
+var path = require('path');
 const port = 8000;
 
-app.listen(port, function () {
-  console.log('Server is running on port ' + port);
-})
+// Using a front-end so I can actually test this
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Allow req.body to work
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
 
 var id;
 var hands =
   [
     {
-      "id" : "0",
+      "id" : "1",
       "cards" :
       [
         {"suit" : "diamonds", "rank" : "4"},
@@ -21,7 +28,7 @@ var hands =
       ]
     },
     {
-      "id" : "1",
+      "id" : "2",
       "cards" :
       [
         {"suit" : "spades", "rank" : "k"},
@@ -37,22 +44,84 @@ app.get("/hands/:id", function(req, res) {
 
     id = req.params.id;
 
-    if (!hands[id]) { res.status(404).send("Error 404 - Not found");}
-    res.status(200).send(hands[id]);
-})
+    if (!hands[id-1]) {
+      return res.status(404).send("Error 404 - Not found");
+    } else {
+      return res.status(200).send(hands[id-1]);
+    }
+});
 
 app.get("/hands/:id/cards", function(req, res) {
 
     id = req.params.id;
 
-    if (!hands[id]) { res.status(404).send("Error 404 - Not found");}
-    res.status(200).send(hands[id]["cards"]);
-})
-//
-// app.post('/hands', function (req, res) {
-//     db.collection('hands').insertOne(post_params, function (err, result) {
-//       if (err) console.log(err);
-//       console.log(result);
-//       res.status(200).send(result[0]["_id"]);
-//     })
+    if (!hands[id-1]) {
+      return res.status(404).send("Error 404 - Not found");
+    } else {
+      return res.status(200).send(hands[id-1]["cards"]);
+    }
+});
+
+// app.post("/hands", function(req, res) {
+//   console.log("calling post");
+//   res.end();
 // });
+
+// app.post("/hands", function (req, res) {
+//
+//     console.log(req.body);
+//     id = hands.length+1;
+//
+//     var singleHand =
+//     [
+//       {"suit" : "spades", "rank" : "k"},
+//       {"suit" : "hearts", "rank" : "j"},
+//       {"suit" : "clubs", "rank" : "q"},
+//       {"suit" : "spades", "rank" : "1"},
+//       {"suit" : "spades", "rank" : "8"}
+//     ]
+//
+//     var hand = {};
+//     var postId = {};
+//
+//     postId.id = id;
+//     hand.id = id;
+//     hand.cards = singleHand;
+//     hands.push(hand);
+//
+//     console.log(hands);
+//     res.send(postId);
+// })
+
+app.put('/hands/:id', function(req,res) {
+  var singleHand =
+      [
+        {"suit" : "spades", "rank" : "6"},
+        {"suit" : "hearts", "rank" : "7"},
+        {"suit" : "clubs", "rank" : "q"},
+        {"suit" : "spades", "rank" : "2"},
+        {"suit" : "spades", "rank" : "k"}
+      ]
+
+      id = req.params.id;
+
+    if (hands[id-1]) {
+
+        console.log("Updating record ID number: " + hands[id-1].id);
+        console.log(hands[id-1].cards);
+
+        hands[id-1].cards = singleHand;
+
+        // Log the update
+        console.log("Updated to:");
+        console.log(hands[id-1].cards);
+
+        res.status(204).send("204: No content");
+    } else {
+        res.status(404).send("404: This hand ID was not found");
+    }
+});
+
+app.listen(port, function () {
+  console.log('Server is running on port ' + port);
+})
